f9e14ca7981e30636309b7213b0738229ee481e4
李玉辉 <liyuhui949@gmail.com>
2025-09-25 16:48:43 +0800

    feat: 解决导入自定义组件库类型声明报错问题

--- packages/elementplus-lib/vite.config.ts
+++ packages/elementplus-lib/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import path from 'path'
-import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
